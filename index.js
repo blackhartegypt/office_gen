3e61a73886f518ffc813c9a963aee18c88ae1a46
blackhartegypt <blackhartegypt@gmail.com>
2024-12-04 09:59:15 +0300

    adding paragraphs and resized images

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const officegen=require("officegen");
 const fs=require("fs");
 const docx=officegen('docx');
 const p=docx.createP();
-p.addText("This should be a simple paragragh");
+p.addText("This should be a simple paragraph");
 const out_put=fs.createWriteStream("results.docx");
-docx.generate(out_put)
+const p2=docx.createP();
+p2.addText("This is the second paragraph");
+docx.putPageBreak();
+const p3=docx.createP();
+p3.addImage("p1.jpg",{cx:100,cy:100});
+docx.generate(out_put);
+
